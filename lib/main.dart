1983ee273d128d5e0a6b9f023ff52044b5c6295d
panchal777 <panchalravina777@gmail.com>
2023-08-13 16:34:51 +0530

    - Added base_library_flutter in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:base_library_flutter/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const LoginPage()
     );
   }
 }
